deccaab2cd4d7c0baf5260f4e3a273c9c759328c
igorface <wojcik.igor.mail@gmail.com>
2020-06-28 17:42:54 +0200

    Followers Dao Impl

--- MicroBlog/src/main/java/Microblog/dao/impl/FollowersDaoImpl.java
+++ MicroBlog/src/main/java/Microblog/dao/impl/FollowersDaoImpl.java
@@ -3,25 +3,43 @@ package Microblog.dao.impl;
 import Microblog.dao.FollowersDao;
 import Microblog.model.user;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
 public class FollowersDaoImpl implements FollowersDao{
 
+	private NamedParameterJdbcTemplate template;
 	@Override
 	public void addFollower(user ownerid, user userid) {
 		Map<String, Object> params = new HashMap<String, Object>();
-        params.put("follower", follower.getId());
-        params.put("followee", followee.getId());
+        params.put("follower", ownerid.getId());
+        params.put("followee", userid.getId());
         
-		String sql = "insert into follower (follower_id, followee_id) values (:follower, :followee)";
+		String hql = "insert into followerslist (follower_id, user_id) values (:ownerid, :userid)";
 		
-        template.update(sql,  params);
-	
-	
-	
-	void addFollower(user ownerid, user userid);
-	
-	void deleteFollower(user ownerid, user userid);
-	
-	void isFollowing(user ownerid, user userid);
-	
+        template.update(hql,  params);
+
+    @Override
+	public void deleteFollower(user ownerid, user userid) {
+		Map<String, Object> params = new HashMap<String, Object>();
+        params.put("follower", ownerid.getId());
+        params.put("followee", userid.getId());
+        
+		String hql = "delete from followerslist (follower_id, user_id) values (:ownerid, :userid)";
+		
+        template.update(hql,  params);
+
+
+    @Override
+	public void addFollower(user ownerid, user userid) {
+		Map<String, Object> params = new HashMap<String, Object>();
+        params.put("follower", ownerid.getId());
+        params.put("followee", userid.getId());
+        
+		String sql = "select count(1) from followerslist where " +
+            "followerslist.ownerid = :follower and followerslist.userid = :followee";
+		
+		Long l = template.queryForObject(sql, params, Long.class);
+		
+		return l > 0;
 	
 }
