85d9172a419d7c24f3383419748b6c0ff9dcb3ac
Andras Kondrat <andrewkondrat@gmail.com>
2020-06-28 17:07:32 +0200

    k

--- MicroBlog/src/main/java/Microblog/dao/impl/PostDaoImpl.java
+++ MicroBlog/src/main/java/Microblog/dao/impl/PostDaoImpl.java
@@ -1,5 +1,43 @@
 package Microblog.dao.impl;
 
-public class PostDaoImpl {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import Microblog.dao.PostDao;
+import Microblog.model.post;
+import Microblog.model.user;
+
+@Repository
+public class PostDaoImpl implements PostDao{
+
+	@Override
+	List<post> retriveUserIdPost(user User_id){
+		
+		
+		String sql = "select posts.*, users.* from posts, users where " +
+				"users.userid = posts.userid and users.userid = :id " +
+				"order by message.pub_date desc";		
+	}
+	
+	@Override
+	List<post> retriveSpecificUsersPosts(user User_id){
+		
+	}
+	
+	@Override
+	List<post> retriveFollowedUsersPosts(user User_id){
+		
+	}
+	
+	@Override
+	List<post> retriveAllUsersPosts(user User_id){
+		
+	}
+	
+	@Override
+	void createPost(post Post) {
+		
+	}
+	
 }
