76354ac86b2a260599b365d7b3a412659a8edc1b
igorface <wojcik.igor.mail@gmail.com>
2020-06-28 17:09:50 +0200

    d

--- MicroBlog/src/main/java/Microblog/dao/impl/FollowersDaoImpl.java
+++ MicroBlog/src/main/java/Microblog/dao/impl/FollowersDaoImpl.java
@@ -1,5 +1,27 @@
 package Microblog.dao.impl;
 
-public class FollowersDaoImpl {
+import Microblog.dao.FollowersDao;
+import Microblog.model.user;
 
+public class FollowersDaoImpl implements FollowersDao{
+
+	@Override
+	public void addFollower(user ownerid, user userid) {
+		Map<String, Object> params = new HashMap<String, Object>();
+        params.put("follower", follower.getId());
+        params.put("followee", followee.getId());
+        
+		String sql = "insert into follower (follower_id, followee_id) values (:follower, :followee)";
+		
+        template.update(sql,  params);
+	
+	
+	
+	void addFollower(user ownerid, user userid);
+	
+	void deleteFollower(user ownerid, user userid);
+	
+	void isFollowing(user ownerid, user userid);
+	
+	
 }
