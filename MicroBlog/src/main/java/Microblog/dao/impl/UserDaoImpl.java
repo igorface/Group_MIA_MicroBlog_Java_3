62905aefdd8199304a920e8401636ed055c539af
ImLazySailor <martyna0010@gmail.com>
2020-06-30 21:22:47 +0200

    UserDao

--- MicroBlog/src/main/java/Microblog/dao/impl/UserDaoImpl.java
+++ MicroBlog/src/main/java/Microblog/dao/impl/UserDaoImpl.java
@@ -1,5 +1,37 @@
 package Microblog.dao.impl;
 
-public class UserDaoImpl {
+import java.util.*;
 
-}
+import javax.persistence.*;
+
+import Microblog.dao.*;
+import Microblog.model.*;
+
+public class UserDaoImpl implements UserDao {
+
+	EntityManager entityManager ;
+
+	    @Override
+	    public user getUserByLogin(String username) {
+	    	String hql = "select * from user where username=:name";
+	    	
+	    	Query query = entityManager.createQuery(hql);
+	    	
+	    	user result ​=​ query​.getSingleResult();
+	    			
+	    	return result;
+	    }
+	    
+	    @Override 
+	    public void registerNewUser(user newuser) {
+	    	String hql = "insert into user (username, userpassword, email) values (:username, :userpassword, :email)";
+					
+			Query query = entityManager.createQuery(hql);
+	    	
+	    	int result ​=​ query​.​executeUpdate​();
+	    	
+			return result;
+	    	
+	    }
+	}
+
