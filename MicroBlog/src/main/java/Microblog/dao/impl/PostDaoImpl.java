2df5d81d932bd57861733112c94c21c65bbaa92c
Andras Kondrat <andrewkondrat@gmail.com>
2020-06-30 20:36:42 +0200

    l

--- MicroBlog/src/main/java/Microblog/dao/impl/PostDaoImpl.java
+++ MicroBlog/src/main/java/Microblog/dao/impl/PostDaoImpl.java
@@ -13,22 +13,23 @@ public class PostDaoImpl implements PostDao{
 	
 	@Override
 	public List<post> retriveUserIdPost(user User_id){
-		
-		Query query = entityManager.createQuery("select post.*, user.* from post, user where " +
-				"user.user_id = post.user_id and user.user_id = :id " +
-				"order by post.date_added desc");
+		String hql = "select post.*, user.* from post, user where " +
+				"user.user_id = post.user_id and user.user_id = " + User_id.user_id +  
+				"order by post.date_added desc";
+				
+		Query query = entityManager.createQuery(hql);
 		List<post> result = query.getResultList();
 		return result;
 	}
 	
 	@Override
 	public List<post> retriveSpecificUsersPosts(user User_id){
-		
 		String hql = "select post.*, user.* from post, user where " +
 				"user.user_id = post.user_id and user.user_id = :id " +
 				"order by post.date_added desc";		
 		
-		List<post> result = null;
+		Query query = entityManager.createQuery(hql);
+		List<post> result = query.getResultList();
 		return result;
 	}
 	
@@ -38,9 +39,9 @@ public class PostDaoImpl implements PostDao{
 				"user.user_id = post.user_id and user.user_id = :id " +
 				"order by post.date_added desc";		
 		
-		List<post> result = null;
+		Query query = entityManager.createQuery(hql);
+		List<post> result = query.getResultList();
 		return result;
-		
 	}
 	
 	@Override
@@ -49,14 +50,19 @@ public class PostDaoImpl implements PostDao{
 				"user.user_id = post.user_id and user.user_id = :id " +
 				"order by post.date_added desc";		
 		
-		List<post> result = null;
+		Query query = entityManager.createQuery(hql);
+		List<post> result = query.getResultList();
 		return result;
-		
 	}
 	
 	@Override
 	public void createPost(post Post) {
-		
+		String hql = "select post.*, user.* from post, user where " +
+				"user.user_id = post.user_id and user.user_id = :id " +
+				"order by post.date_added desc";	
+
+		Query query = entityManager.createQuery(hql);
+		query.executeUpdate();
 	}
 	
 }
