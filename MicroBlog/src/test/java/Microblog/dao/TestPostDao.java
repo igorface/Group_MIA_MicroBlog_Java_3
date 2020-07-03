3314385f889809bb4d1f8eb5681b46f05d206f7c
ImLazySailor <51158316+ImLazySailor@users.noreply.github.com>
2020-07-03 20:25:52 +0200

    Create TestPostDao.java

---
+++ MicroBlog/src/test/java/Microblog/dao/TestPostDao.java
@@ -0,0 +1,32 @@
+package Microblog.dao;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import Microblog.dao.*;
+import Microblog.model.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext-test.xml"})
+@Transactional
+@Rollback(true)
+public class TestPostDao {
+   
+	@Autowired
+	PostDao postDAO;
+	
+	@Autowired
+	UserDao userDAO;
+    
+    }
+
+
